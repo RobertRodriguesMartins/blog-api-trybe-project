597014988000fdeccf7d08f649b59296530aa2c4
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-14 15:24:11 -0300

    FEAT<services>: deal with login, calls model

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,7 +1,19 @@
 const models = require('../database/models');
 
 const loginService = {
-  login: async (user) => {},
+  /**
+   * @param {{email: string, password: string}} requestUser
+   */
+  login: async (requestUser) => {
+    const user = await models.User.findOne({
+      where: {
+        email: requestUser.email,
+        password: requestUser.password,
+      },
+    });
+
+    return user;
+  },
 };
 
 module.exports = loginService;
