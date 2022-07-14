1555c149a7a0661de89c02f3b922b8b370d77bdf
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-14 19:40:15 -0300

    FEAT<schemas>: new schemas available

--- src/utils/schemas/index.js
+++ src/utils/schemas/index.js
@@ -1,5 +1,9 @@
 const loginSchemas = require('./loginSchemas');
+const userSchemas = require('./userSchemas');
+const tokenSchemas = require('./tokenSchemas');
 
 module.exports = {
   ...loginSchemas,
+  ...userSchemas,
+  ...tokenSchemas,
 };
