54e564a8016158f4808a66c110d233483a987da6
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-14 20:57:50 -0300

    UPDATE<schemas>: handler update

--- src/utils/schemas/index.js
+++ src/utils/schemas/index.js
@@ -1,9 +1,11 @@
 const loginSchemas = require('./loginSchemas');
 const userSchemas = require('./userSchemas');
 const tokenSchemas = require('./tokenSchemas');
+const categorySchemas = require('./categorySchemas');
 
 module.exports = {
   ...loginSchemas,
   ...userSchemas,
   ...tokenSchemas,
+  ...categorySchemas,
 };
