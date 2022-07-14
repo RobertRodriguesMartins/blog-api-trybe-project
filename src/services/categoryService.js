4005dcd781f159ef1fa32b56c1e951c4c8964f89
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-14 20:58:20 -0300

    FEAT<services>: category entity

---
+++ src/services/categoryService.js
@@ -0,0 +1,40 @@
+const models = require('../database/models');
+
+const userService = {
+  /**
+   * @param {{name: string}} categoryObject
+   */
+  findByName: async (categoryObject) => {
+    const category = await models.Category.findOne({
+      where: {
+        name: categoryObject.name,
+      },
+    });
+
+    return category;
+  },
+  findAll: async () => {
+    const categories = await models.Category.findAll({
+      raw: true,
+    });
+
+    return categories;
+  },
+  findById: async (id) => {
+    const category = await models.Category.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return category;
+  },
+  create: async (categoryObject) => {
+    const { dataValues: category } = await models.Category.create(
+      categoryObject
+    );
+    return category;
+  },
+};
+
+module.exports = userService;
