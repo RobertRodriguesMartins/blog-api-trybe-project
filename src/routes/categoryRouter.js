918320810fe8eb44d4f366b974cc139e805b5ae0
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-14 21:09:18 -0300

    FEAT<routes>: GET /categories

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,6 +2,8 @@ const Router = require('express').Router();
 const { categoryController } = require('../controllers');
 const { tokenMiddleware } = require('../middlewares');
 
-Router.use(tokenMiddleware).post('/', categoryController.create);
+Router.use(tokenMiddleware)
+  .post('/', categoryController.create)
+  .get('/', categoryController.findAll);
 
 module.exports = Router;
