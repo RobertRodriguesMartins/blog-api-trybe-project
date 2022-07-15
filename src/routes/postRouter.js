39df5954fef24050047c36b5f9ec25fe09b6a65b
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-15 11:11:27 -0300

    FEAT<routes>: GET TO /post

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -2,6 +2,8 @@ const Router = require('express').Router();
 const { postController } = require('../controllers');
 const { tokenMiddleware } = require('../middlewares');
 
-Router.use(tokenMiddleware).post('/', postController.create);
+Router.use(tokenMiddleware)
+  .post('/', postController.create)
+  .get('/', postController.findAll);
 
 module.exports = Router;
